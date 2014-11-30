72178bcbbd600c949d3f494dc04e1fda5dffef42
Michael D. Hoyle <hoylemd@gmail.com>
2014-11-30 15:33:33 -0500

    making migrations

---
+++ brokenmigration/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Thing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ThingList',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('things', models.ManyToManyField(to='brokenmigration.Thing')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
