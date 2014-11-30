b43039bf75d4f34189273cba6b1aa4dd4f0f98a5
Michael D. Hoyle <hoylemd@gmail.com>
2014-11-30 15:33:08 -0500

    fixed some bade codes

--- brokenmigration/models.py
+++ brokenmigration/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Thing(models.Model);
+class Thing(models.Model):
     name = models.CharField(max_length=200)
 
-class ThingList
+class ThingList(models.Model):
     name = models.CharField(max_length=200)
     things = models.ManyToManyField('Thing')
